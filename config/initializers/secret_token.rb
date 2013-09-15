2cf98895046daca51a04bd40d63752fe97de428b
George MacRorie <george.macrorie@semantico.com>
2013-09-15 16:29:51 +0100

    Initial Rails Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoSeek::Application.config.secret_key_base = '5b757e253a30584ac00d1b262931f1522f57ac7d409712a70689a88e5e5b2659f4565d298f83913d4f82ecc28802fac3f4efd06fab9df052a002acb4ad2195cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GoSeek::Application.config.session_store :cookie_store, key: '_go-seek_session'
